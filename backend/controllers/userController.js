648f04847bb292aef7b47518adc00cbe7d20379d
William Pasternak <wjp09@yahoo.com>
2024-02-16 12:50:17 -0500

    feat: Implement logout functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -114,7 +114,23 @@ const loginUser = asyncHandler(async (req,res) => {
 })
 
 
+// Logout user
+const logout = asyncHandler(async (req, res) => {
+    res.cookie("token", "", {
+        path: "/",
+        httpOnly: true,
+        expires: new Date(0),
+        sameSite: "none",
+        secure: true,
+      })
+      return res.status(200).json({
+        message: 'Successfully Logged Out'
+      })
+})
+
+
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    logout
 }
