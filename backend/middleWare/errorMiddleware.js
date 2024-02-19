5dc20cdd5b502d7221954ed07048fd3dd2ed9225
William Pasternak <wjp09@yahoo.com>
2024-02-19 00:00:22 -0500

    feat: Implement password reset functionality

--- backend/middleWare/errorMiddleware.js
+++ backend/middleWare/errorMiddleware.js
@@ -1,9 +1,7 @@
 const errorHandler = (err, req, res, next) => {
 
     const statusCode = res.statusCode ? res.statusCode : 500
-    res.status(statusCode)
-
-    res.json({
+    res.status(statusCode).json({
         message: err.message,
         // Display error stack in development mode only.
         stack: process.env.NODE_ENV === 'development' ? err.stack : null
